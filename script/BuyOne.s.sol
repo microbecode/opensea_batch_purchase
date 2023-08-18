40f4e17f3ed2b54d4753d17e2645e405574d18fb
Lauri Peltonen <20242241+microbecode@users.noreply.github.com>
2023-08-18 18:44:01 +0300

    add addresses

--- script/BuyOne.s.sol
+++ script/BuyOne.s.sol
@@ -5,5 +5,9 @@ import { BaseScript } from "./Base.s.sol";
 
 /// @dev See the Solidity Scripting tutorial: https://book.getfoundry.sh/tutorials/solidity-scripting
 contract BuyOne is BaseScript {
+    address public seaport = 0x00000000000000ADc04C56Bf30aC9d3c0aAF14dC; // seaport 1.5
+    address public nft = 0x8a90CAb2b38dba80c64b7734e58Ee1dB38B8992e; // Doodle token
+    uint256 public tokenId = 7484;
+
     function run() public broadcast { }
 }
