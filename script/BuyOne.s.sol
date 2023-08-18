9e20c47c356414d1e0155e2c373d1e95ee4e7fd6
Lauri Peltonen <20242241+microbecode@users.noreply.github.com>
2023-08-18 18:23:07 +0300

    add fork and run

---
+++ script/BuyOne.s.sol
@@ -0,0 +1,9 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity >=0.8.19 <=0.9.0;
+
+import { BaseScript } from "./Base.s.sol";
+
+/// @dev See the Solidity Scripting tutorial: https://book.getfoundry.sh/tutorials/solidity-scripting
+contract BuyOne is BaseScript {
+    function run() public broadcast { }
+}
