4a2f002829b29c5445c0198d5276bb70ee50a0ac
Lauri Peltonen <20242241+microbecode@users.noreply.github.com>
2023-08-19 16:20:43 +0300

    minor

--- script/BuyOne.s.sol
+++ script/BuyOne.s.sol
@@ -2,7 +2,7 @@
 pragma solidity >=0.8.17 <=0.9.0;
 
 import { BaseScript } from "./Base.s.sol";
-import "seaport-core/lib/Consideration.sol";
+import { Consideration } from "seaport-core/lib/Consideration.sol";
 
 /// @dev See the Solidity Scripting tutorial: https://book.getfoundry.sh/tutorials/solidity-scripting
 contract BuyOne is BaseScript {
